17ac43bcee0a0bd17b86915060eec486e29aa081
samlimby <129619263+samlimby@users.noreply.github.com>
2024-07-21 19:03:38 +0100

    Seconds input - script

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const timeSecond = 0;
-const timeMinute = timeSecond * 60;
-const timer = document.querySelector('.main_block-timer-title');
+let timeSecond = 0;
+let timeMinute = timeSecond * 60;
+let timerCount = timeMinute + ":" + timeSecond;
 
 function start() {
-    console.log("this is working");
+    timeSecond++;
+    setInterval(start, 1000);
+    console.log(timerCount);
+    return document.getElementById("timer-count").innerHTML = timeSecond;
 }
 
-function startTimer() {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    timer.textContent = `${minutes}:${seconds}`;
-}
+
+
