23352c801fcc0197e0b1f5a4a5b26f0191f189df
samlimby <129619263+samlimby@users.noreply.github.com>
2024-07-22 22:13:57 +0100

    counting functionality

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+const pomoInterval = setInterval(startTimer,1000);
 let timeSecond = 0;
 let timeMinute = timeSecond * 60;
-let timerCount = timeMinute + ":" + timeSecond;
 
-function start() {
-    timeSecond++;
-    setInterval(start, 1000);
-    console.log(timerCount);
-    return document.getElementById("timer-count").innerHTML = timeSecond;
-}
+function startTimer() {
+    timeSecond++; 
+    if (timeSecond > 59) {
+        timeSecond = 0;
+        timeMinute++;
+    }
+    const timerCount = timeMinute + ":" + timeSecond;
+    if (timeMinute === 25) {
+        alert("Time is up!");
+        return stopTimer();
+    }
+    return document.getElementById("timer-count").innerText = timerCount;
+}; 
+
+function stopTimer() {
+    timeSecond = 0;
+    timeMinute = 0;
+    clearInterval(pomoInterval);
+    return document.getElementById("timer-count").innerText = "00:00";
+};
 
 
 
