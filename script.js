155a59ebccca77fb88fa24efac725e3869a46fb1
samlimby <129619263+samlimby@users.noreply.github.com>
2024-07-24 19:37:21 +0100

    Browser Tab Title Update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let pomoInterval;
 let timeSecond = 0;
 let timeMinute = 0;
+const originalTitle = document.title;
 
 function startTimer() {
     pomoInterval = setInterval(updateTimer, 1000);
@@ -14,6 +15,7 @@ function updateTimer() {
     }
     const timerCount = `${String(timeMinute).padStart(2, '0')}:${String(timeSecond).padStart(2, '0')}`;
     document.getElementById("timer-count").innerText = timerCount;
+    document.title = timerCount;
 
     if (timeMinute === 25) {
         alert("Time is up!");
@@ -26,6 +28,7 @@ function stopTimer() {
     timeSecond = 0;
     timeMinute = 0;
     document.getElementById("timer-count").innerText = "00:00";
+    document.title = originalTitle; 
 }
 
 
